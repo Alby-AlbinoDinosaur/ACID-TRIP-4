8bd90ab753f7b04000c89cbe0f5bdb8417d6f599
Esteban <44073856+ElEstebann@users.noreply.github.com>
2022-02-16 02:06:07 -0800

    Damage Texts
    Included creation of damage indicators on EnemyGUI and playerGUI.
    Also created text to indicate player hp and pp stats, both updated by playerGUI. Currently, playerGUI inaccurately obtains a player's max hp because of the way its initialized.

--- AcidTrip4/Assets/Scripts/Battle/UI/PlayerGUI.cs
+++ AcidTrip4/Assets/Scripts/Battle/UI/PlayerGUI.cs
@@ -11,7 +11,12 @@ public class PlayerGUI : MonoBehaviour
     public Entity playerEntity;
     public Image hpBar;
     public Image ppBar;
+    public TextMeshProUGUI hpText;
+    public TextMeshProUGUI ppText;
     public TextMeshProUGUI nextText;
+    public GameObject damagePrefab;
+    public GameObject healPrefab;
+    
 
     public Button attackButton;
     public Button itemsButton;
@@ -22,6 +27,7 @@ public class PlayerGUI : MonoBehaviour
     public Animator faceAnimator;
 
     private int maxHp = 0;
+    private int maxPpl = 0;
     private int currentHp = 0;
     private int maxPp = 0;
     void Start()
@@ -35,6 +41,8 @@ public class PlayerGUI : MonoBehaviour
         
         //Debug.Log("maxHp set to " + maxHp);
         maxPp = playerEntity.power_points;
+        
+        updateText();
 
     }
 
@@ -67,22 +75,22 @@ public class PlayerGUI : MonoBehaviour
     private void updateFace(float ratio){
         if(ratio > (float)2/3)
                     {   
-                        Debug.Log("Set State 0");
+                        //Debug.Log("Set State 0");
                         faceAnimator.SetInteger("State", 0);
                     }
                     else if(ratio > (float)1/3)
                     {   
-                        Debug.Log("Set State 1");
+                        //Debug.Log("Set State 1");
                         faceAnimator.SetInteger("State", 1);
                     }
                     else if(ratio > (float)0)
                     {   
-                        Debug.Log("Set State 2");
+                        //Debug.Log("Set State 2");
                         faceAnimator.SetInteger("State", 2);
                     }
                     else 
                     {   
-                        Debug.Log("Set State 3");
+                        //Debug.Log("Set State 3");
                         faceAnimator.SetInteger("State", 3);
                     }
                     
@@ -94,14 +102,20 @@ public class PlayerGUI : MonoBehaviour
         if(maxHp != 0)
         {
 
+            
+
             if(currentHp != playerEntity.health_stat)
             {
                 
+
+
                 float ratio = (float)playerEntity.health_stat/maxHp;
                 hpBar.fillAmount = ratio;
+                /*
                 Debug.Log("Ratio: " +ratio);
                 Debug.Log("Current HP: " + currentHp);
                 Debug.Log("Actual HP: " + playerEntity.health_stat);
+                */
                 
                 if(playerEntity.health_stat < currentHp)
                 {
@@ -112,11 +126,21 @@ public class PlayerGUI : MonoBehaviour
                     updateFace(ratio);
                     
                     
+                    GameObject canvas = Instantiate(damagePrefab, hpBar.rectTransform.position, Quaternion.identity);
+                    canvas.transform.SetParent(gameObject.transform);
+                    TextMeshProUGUI text = canvas.transform.GetChild(0).gameObject.GetComponent(typeof(TextMeshProUGUI)) as TextMeshProUGUI;
+                    text.text = (currentHp-playerEntity.health_stat).ToString();
+                    
+                    
                     
                 }
                 else
                 {
                     faceAnimator.SetTrigger("Heal");
+                    GameObject canvas = Instantiate(healPrefab, hpBar.rectTransform.position, Quaternion.identity);
+                    canvas.transform.SetParent(gameObject.transform);
+                    TextMeshProUGUI text = canvas.transform.GetChild(0).gameObject.GetComponent(typeof(TextMeshProUGUI)) as TextMeshProUGUI;
+                    text.text = (playerEntity.health_stat-currentHp).ToString();
 
                     updateFace(ratio);
                 }
@@ -131,6 +155,17 @@ public class PlayerGUI : MonoBehaviour
             ppBar.fillAmount = playerEntity.power_points/maxPp;
         }
 
+        updateText();
+
+        
+        //nextText.text = "1";
+        
+    }
+    void updateText()
+    {
+        hpText.text = playerEntity.health_stat.ToString() + "/" + maxHp.ToString();
+        ppText.text = playerEntity.power_points.ToString() + "/" + maxPp.ToString();
+
         if(playerEntity.nextMove >=0 && !playerEntity.nextMoveHasAlreadyBeenRun)
         {
             nextText.text = playerEntity.moveTextList[playerEntity.nextMove](0);
@@ -138,8 +173,7 @@ public class PlayerGUI : MonoBehaviour
         else{
             nextText.text = "";
         }
-        //nextText.text = "1";
-        
+
     }
 
 
--- AcidTrip4/Assets/Scripts/Battle/enemyGUI.cs
+++ AcidTrip4/Assets/Scripts/Battle/enemyGUI.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class enemyGUI : MonoBehaviour
 {
@@ -9,10 +10,16 @@ public class enemyGUI : MonoBehaviour
     // Start is called before the first frame update
     public Image hpBar;
     
+    
     public Button selector;
+    public GameObject damagePrefab;
+
+    
 
     public Entity enemyEntity;
     private int maxHp = 0;
+    
+    private int currentHp = 0;
 
     void Start()
     {
@@ -21,6 +28,9 @@ public class enemyGUI : MonoBehaviour
         BattleEventManager.OnGUIUpdate+= updateGUI;
         selector.interactable = false;
         maxHp = enemyEntity.health_stat;
+        currentHp = maxHp;
+        
+
     }
 
     // Update is called once per frame
@@ -43,7 +53,21 @@ public class enemyGUI : MonoBehaviour
     {
         if(maxHp != 0)
         {
+            Debug.Log("CALLED CURRENHP = " + currentHp + "ENEMY HP: " + enemyEntity.health_stat);
+
             hpBar.fillAmount = (float)enemyEntity.health_stat/maxHp;
+            if(enemyEntity.health_stat<currentHp){
+                Debug.Log("DAMAGED");
+                GameObject canvas = Instantiate(damagePrefab, gameObject.transform.position, Quaternion.identity);
+                //canvas.transform.parent = gameObject.transform;
+                TextMeshProUGUI text = canvas.transform.GetChild(0).gameObject.GetComponent(typeof(TextMeshProUGUI)) as TextMeshProUGUI;
+                text.text = (enemyEntity.health_stat-currentHp).ToString();
+            }
+            currentHp = enemyEntity.health_stat;
+            
         }
+        
     }
+
+    
 }
