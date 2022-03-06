68707d19931d3baf1676860054635f38b7f98929
Esteban <44073856+ElEstebann@users.noreply.github.com>
2022-03-06 15:19:00 -0800

    Spruced Up Title Screen

--- AcidTrip4/Assets/LoadDemo.cs
+++ AcidTrip4/Assets/LoadDemo.cs
@@ -9,7 +9,7 @@ public class LoadDemo : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        AudioManager.instance.Play("title");
     }
 
     // Update is called once per frame
@@ -20,6 +20,15 @@ public class LoadDemo : MonoBehaviour
 
     public void loadScene(int sceneNo)
     {
+        
+        //SceneManager.LoadScene(sceneNo);
+        StartCoroutine(transition(sceneNo));
+    }
+
+    IEnumerator transition(int sceneNo){
+
+        yield return new WaitForSeconds(1.1f);
+        AudioManager.instance.Stop("title");
         SceneManager.LoadScene(sceneNo);
     }
 }
