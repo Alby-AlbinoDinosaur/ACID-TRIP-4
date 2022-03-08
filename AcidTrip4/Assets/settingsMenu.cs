5d0763b3d9aa8402abfb8718f80c8adb1de63e8d
Esteban <44073856+ElEstebann@users.noreply.github.com>
2022-03-07 19:59:07 -0800

    Shreddar + Pause Menu Sfx fix

---
+++ AcidTrip4/Assets/settingsMenu.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class settingsMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    public void pause(){
+
+    }
+    public void loadScene(int sceneNo)
+    {
+        
+        //SceneManager.LoadScene(sceneNo);
+        StartCoroutine(transition(sceneNo));
+    }
+    IEnumerator transition(int sceneNo){
+
+        yield return new WaitForSeconds(1.1f);
+        //AudioManager.instance.Stop("title");
+        SceneManager.LoadScene(sceneNo);
+    }
+    void Update()
+    {
+        
+    }
+}
